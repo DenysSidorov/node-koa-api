82d7a58ec15b012c286930f16f5346bab35d54f1
Denys Sidorov <000scorpions000@gmail.com>
2018-03-13 22:16:40 +0200

    implement logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,9 +4,13 @@ var ObjectId = mongoose.Types.ObjectId;
 
 
 module.exports.logIn = async (ctx, next) => {
+
   ctx.body = 'Beginning! logIn'
 }
 
 module.exports.logOut= async (ctx, next) => {
+  ctx.logout();
+  ctx.session = null; // destroy session (!!!)
+  ctx.redirect('/');
   ctx.body = 'Beginning! logOut'
 }
