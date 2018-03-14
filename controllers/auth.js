ca5cac5da27d195a8e8945b45a4479bd3ff991c1
Denys Sidorov <000scorpions000@gmail.com>
2018-03-14 22:49:33 +0200

    implement login method in app

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,26 @@
 const User = require('../models/user');
 const mongoose = require('../connection/index');
 var ObjectId = mongoose.Types.ObjectId;
+const passport = require('koa-passport');
 
 
 module.exports.logIn = async (ctx, next) => {
+
+  // опции @https://github.com/jaredhanson/passport/blob/master/lib/middleware/authenticate.js
+  // можно передать и функцию
+  // имя local паспорт берет из локальной стратеции из мидлвер
+  await passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/',
+    //failureMessage: true // запишет сообщение об ошибке в session.messages[]
+    failureFlash: true // req.flash, better
+
+    // assignProperty: 'something' присвоить юзера в свойство req.something
+    //   - нужно для привязывания акков соц. сетей
+    // если не стоит, то залогинит его вызовом req.login(user),
+    //   - это поместит user.id в session.passport.user (если не стоит опция session:false)
+    //   - также присвоит его в req.user
+  })(ctx, next);
 
   ctx.body = 'Beginning! logIn'
 }
